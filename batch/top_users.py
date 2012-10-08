ee606a37982dde21b377e071fdb613bee68a2204
Fred Song <fsong@xei.ca>
2012-10-07 19:58:01 -0700

    finished batch and added template for top_users feature

--- batch/top_users.py
+++ batch/top_users.py
@@ -1,47 +1,39 @@
 from datetime import datetime
 
 from django.db import connection, transaction
+from django.db.models import Count
 
 from batch.models import TopUsersTopic, TopUsersPost, TopUsersBatch
-from gfaqs.models import User
+from gfaqs.models import User, Topic, Post
 
 
 NUM_TOP_USERS=50
 
-def dictfetchall(cursor):
-    "Returns all rows from a cursor as a dict"
-    desc = cursor.description
-    return [
-        dict(zip([col[0] for col in desc], row))
-        for row in cursor.fetchall()
-    ]
-
-
 class Batch(object):
-    pass
-
-class TopUsersBatch(Batch):
+    """ Abstract class for batches """
     def start(self):
-        top_users_posts = self.get_top_users_posts()
-        #top_users_topics = self.get_top_users_topics()
-        import pdb; pdb.set_trace()
+        """ Entry point to batch; override this method in subclass """
         pass
 
+class TopUsersBatch(Batch):
+    @transaction.commit_on_success
+    def start(self):
+        top_users_posts = self.get_top_users_posts()
+        top_users_topics = self.get_top_users_topics()
+
+        batch = TopUsersBatch(date=datetime.now())
+        for user in top_users_topics:
+            tut = TopUserTopic(batch=batch,
+                username=user.username, count=user.num_topics)
+            tut.save()
+
+        for user in top_users_topics:
+            tup = TopUserPost(batch=batch,
+                username=user.username, count=user.num_posts)
+            tup.save()
+
     def get_top_users_posts(self):
-        #TODO: use django ORM to do this rather than raw sql...
-        sql = "select gfaqs_user.id as creator_id, username, tcount from gfaqs_user JOIN " \
-            "(select creator_id, count(*) as tcount from gfaqs_topic group by " \
-            "gfaqs_topic.id) as foo order by tcount desc limit %s;" % NUM_TOP_USERS
-        cursor = connection.cursor() 
-        cursor.execute(sql)
-        return dictfetchall(cursor)
+        return User.objects.annotate(num_posts=Count("post"))[:NUM_TOP_USERS]
 
     def get_top_users_topics(self):
-        #TODO: use django ORM to do this rather than raw sql...
-        sql = "select gfaqs_user.id as creator_id,username,pcount " \
-            "from users natural join " \
-            "(select creator_id, count(*) as pcount from posts group by user_id) " \
-            "as foo order by pcount desc limit %s;" % NUM_TOP_USERS;
-        cursor = connection.cursor() 
-        cursor.execute(sql)
-        return dictfetchall(cursor)
+        return User.objects.annotate(num_topics=Count("topic"))[:NUM_TOP_USERS]
