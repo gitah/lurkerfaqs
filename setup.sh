d700e125e3aec99c9520ac67e46aaa6314e59450
Fred Song <fsong@xei.ca>
2012-11-17 15:04:19 -0800

    Added alias in httpd.conf for static files

--- setup.sh
+++ setup.sh
@@ -64,6 +64,8 @@ cat <<HTTPDCONF > /etc/apache2/httpd.conf
 WSGIScriptAlias / $PROJECT_ROOT/lurkerfaqs/wsgi.py
 WSGIPythonPath $PROJECT_ROOT
 
+alias /static $PROJECT_ROOT/lurkerfaqs/static
+
 <Directory $PROJECT_ROOT>
 Order allow,deny
 Allow from all
