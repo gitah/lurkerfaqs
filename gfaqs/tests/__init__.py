0e6600f7db2d6caad7fcb5ed1d04a6ba50203525
Fred Song <fsong@xei.ca>
2012-11-17 16:34:20 -0800

    added gamefaqs DOM monitoring tests

--- gfaqs/tests/__init__.py
+++ gfaqs/tests/__init__.py
@@ -1,1 +1,2 @@
-from tests import BoardScraperTest, TopicScraperTest, ArchiverTest
+from tests import *
+from monitoring_tests import *
---
+++ gfaqs/tests/monitoring_tests.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""These tests are integration tests that hits the actual gfaqs website to make
+sure the structure is compatible with the archiver"""
+
+from django.conf import settings
+from django.test import TestCase
+
+from gfaqs.login import authenticate
+from gfaqs.models import User, Board, Topic, Post
+from gfaqs.scraper import BoardScraper, TopicScraper
+
+CE = Board(url="%s/boards/400-current-events" % settings.GFAQS_URL, name="CE")
+OT = Board(url="%s/boards/2000121-anime-and-manga-other-titles" % settings.GFAQS_URL, name="OT")
+
+class GFAQSBoardTest(TestCase):
+    """Tests that the DOM of gfaqs message boards has not changed."""
+
+    def test_scrape_board(self):
+        # scrape board
+        bs = BoardScraper(OT)
+        topic = bs.retrieve().next()
+        self.assertTrue(topic)
+        self.assertTrue(topic.title)
+        self.assertTrue(topic.creator.username)
+
+        # scrape topic
+        ts = TopicScraper(topic)
+        post = ts.retrieve().next()
+        self.assertTrue(post)
+        self.assertTrue(post.contents)
+        self.assertTrue(post.creator.username)
+
+
+class GFAQSLoginTest(TestCase):
+    """Tests that we can still login to GameFAQs."""
+
+    def test_login(self):
+        if not settings.GFAQS_LOGIN_AS_USER:
+            return
+        opener = authenticate(settings.GFAQS_LOGIN_EMAIL, settings.GFAQS_LOGIN_PASSWORD)
+
+        # see that we can see topics on CE
+        bs = BoardScraper(CE)
+        topic = bs.retrieve(opener).next()
+        self.assertTrue(topic)
+        self.assertTrue(topic.title)
+        self.assertTrue(topic.creator.username)
