35bde8f61792cdbef82c9cf5fbc27e55a570e37c
Fred Song <fsong@xei.ca>
2012-11-23 12:16:22 -0800

    Removing a composite index for (user_id, last_post_date) on gfaq_topics
    Found index unneccesary

--- sql/create_index.sql
+++ sql/create_index.sql
@@ -1,3 +1,2 @@
 CREATE INDEX topic_composite_date ON gfaqs_topic(board_id, last_post_date);
-CREATE INDEX topic_creator_composite_date ON gfaqs_topic(creator_id, last_post_date);
 CREATE INDEX post_composite_date ON gfaqs_post(creator_id, date);
