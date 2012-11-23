fd2bb8f12d6326de830ed0a0e908b309ca6790a6
Fred Song <fsong@xei.ca>
2012-11-23 12:06:28 -0800

    cron now append to log rather than over write it

--- setup/cron_setup.sh
+++ setup/cron_setup.sh
@@ -13,9 +13,9 @@ touch $LURKERFAQS_CRON_USER_COUNT
 chmod 755 $LURKERFAQS_CRON_USER_COUNT
 cat <<CRON > $LURKERFAQS_CRON_USER_COUNT
 #!/bin/sh
-echo [\`date\`] 'Batch User Counts Starting' 1> $BATCH_LOG 2>&1
-$PROJECT_ROOT/manage.py batch_user_counts 1> $BATCH_LOG 2>&1
-echo [\`date\`] 'Batch User Counts Ended' 1> $BATCH_LOG 2>&1
+echo [\`date\`] 'Batch User Counts Starting' 1>> $BATCH_LOG 2>&1
+$PROJECT_ROOT/manage.py batch_user_counts 1>> $BATCH_LOG 2>&1
+echo [\`date\`] 'Batch User Counts Ended' 1>> $BATCH_LOG 2>&1
 CRON
 
 # Topic indexing batch
@@ -24,10 +24,10 @@ touch $LURKERFAQS_CRON_INDEX_TOPICS
 chmod 755 $LURKERFAQS_CRON_INDEX_TOPICS
 cat <<CRON > $LURKERFAQS_CRON_INDEX_TOPICS
 #!/bin/sh
-echo [\`date\`] 'Batch Index Topics Starting' 1> $BATCH_LOG 2>&1
-$PROJECT_ROOT/manage.py batch_index_topics update 1> $BATCH_LOG 2>&1
-service jetty restart 1> $BATCH_LOG 2>&1
-echo [\`date\`] 'Batch Index Topics Ended' 1> $BATCH_LOG 2>&1
+echo [\`date\`] 'Batch Index Topics Starting' 1>> $BATCH_LOG 2>&1
+$PROJECT_ROOT/manage.py batch_index_topics update 1>> $BATCH_LOG 2>&1
+service jetty restart 1>> $BATCH_LOG 2>&1
+echo [\`date\`] 'Batch Index Topics Ended' 1>> $BATCH_LOG 2>&1
 CRON
 
 # GFAQs DOM monitoring
@@ -36,14 +36,14 @@ touch $LURKERFAQS_CRON_MONITORING
 chmod 755 $LURKERFAQS_CRON_MONITORING
 cat <<CRON > $LURKERFAQS_CRON_MONITORING
 #!/bin/sh
-echo [\`date\`] 'DOM tests started' 1> $BATCH_LOG 2>&1
-$PROJECT_ROOT/manage.py test gfaqs.GFAQSDOMTest 1> $BATCH_LOG 2>&1
+echo [\`date\`] 'DOM tests started' 1>> $BATCH_LOG 2>&1
+$PROJECT_ROOT/manage.py test gfaqs.GFAQSDOMTest 1>> $BATCH_LOG 2>&1
 if [ $? -ne 0 ];
 then
-    echo [\`date\`] 'Test failed; sending email to admin' 1> $BATCH_LOG 2>&1
-    $PROJECT_ROOT/manage.py alert_admin 'gfaqs dom test failed' 'failed' 1> $BATCH_LOG 2>&1
+    echo [\`date\`] 'Test failed; sending email to admin' 1>> $BATCH_LOG 2>&1
+    $PROJECT_ROOT/manage.py alert_admin 'gfaqs dom test failed' 'failed' 1>> $BATCH_LOG 2>&1
 fi
-echo [\`date\`] 'DOM tests ended' 1> $BATCH_LOG 2>&1
+echo [\`date\`] 'DOM tests ended' 1>> $BATCH_LOG 2>&1
 CRON
 
 # Database Backup
@@ -52,7 +52,7 @@ touch $LURKERFAQS_CRON_BACKUPDB
 chmod 755 $LURKERFAQS_CRON_BACKUPDB
 cat <<CRON > $LURKERFAQS_CRON_BACKUPDB
 #!/bin/sh
-echo [\`date\`] 'Backing up database!' 1> $BATCH_LOG 2>&1
-$PROJECT_ROOT/manage.py backupdb 1> $BATCH_LOG 2>&1
-echo [\`date\`] 'Backup database finished!' 1> $BATCH_LOG 2>&1
+echo [\`date\`] 'Backing up database!' 1>> $BATCH_LOG 2>&1
+$PROJECT_ROOT/manage.py backupdb 1>> $BATCH_LOG 2>&1
+echo [\`date\`] 'Backup database finished!' 1>> $BATCH_LOG 2>&1
 CRON
