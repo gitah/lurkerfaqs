495e0bb84caaf93dd74acf8a5ab2ec04dffb232f
Fred Song <fsong@xei.ca>
2015-04-01 10:47:06 -0400

    Finish JS for donate button

--- lurkerfaqs/static/js/main.js
+++ lurkerfaqs/static/js/main.js
@@ -1,5 +1,13 @@
-POPUP_MODAL = "#deletePostModal";
-DONATE_BUTTON = "#donateButton";
+"use strict";
+
+var POPUP_MODAL = "#deletePostModal";
+var DONATE_BUTTON = "#donateButton";
+
+var CREATE_PAYMENTS_DELETE_POST_URL = "/payment/delete_post";
+
+var getCreatePaymentDeletePostUrl = function(topic_id, post_num) {
+        return CREATE_PAYMENTS_DELETE_POST_URL + "/" + topic_id + "/" + post_num;
+}
 
 var showDeletePostPopup = function(topic_id, post_num) {
     var popupModal$ = $(POPUP_MODAL);
@@ -23,8 +31,17 @@ $(function() {
     });
 
     $(DONATE_BUTTON).click(function(event) {
-        // Ajax call to API
-        // Parse auth_url
-        // Forward to auth_url
+        var data = $(POPUP_MODAL).data('post_id');
+        var topic_id = data['topic_id'];
+        var post_num = data['post_num'];
+
+        $(this).html('<i class="fa fa-spinner fa-spin"></i>');
+        $(this).prop('disabled', true)
+
+        var url = getCreatePaymentDeletePostUrl(topic_id, post_num);
+        $.getJSON(url, function(resp) {
+            var approvalUrl = resp["approval_url"];
+            window.location.replace(approvalUrl);
+        });
     });
 })
